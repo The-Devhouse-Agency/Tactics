e48468cbbc666f82528c6866546bd8c5c5d73d6b
Jim Welch <jim@thedevhouseagency.com>
2022-02-21 12:14:55 -0600

    spacebar updates map

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -17,18 +17,23 @@ public class LevelGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GenerateWorld();
+        }
     }
 
     void GenerateWorld()
     {
+        foreach (Transform child in gridHolder.transform)
+            Destroy(child.gameObject);
+
         for (int i = 0; i < gridSize; i++)
         {
             GameObject newSquare = Instantiate(floorTilePrefabs[Random.RandomRange(0, 3)], new Vector3(i,0,0), Quaternion.identity, gridHolder.transform);
             for (int j = 0; j < gridSize; j++)
             {
                 GameObject newSquare2 = Instantiate(floorTilePrefabs[Random.RandomRange(0, 3)], new Vector3(i, 0, j), Quaternion.identity, gridHolder.transform);
-
             }
         }
     }
