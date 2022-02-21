6e9116f6c1844cae37690655d84a8da4bdf5bc94
jrivera26333 <jrivera26333@gmail.com>
2022-02-21 12:07:28 -0600

    Started Character Class

--- Assets/Scripts/Warrior.cs
+++ Assets/Scripts/Warrior.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Warrior : MonoBehaviour
+public class Warrior : Character
 {
+    public override int attackDamage { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
     // Start is called before the first frame update
     void Start()
     {
